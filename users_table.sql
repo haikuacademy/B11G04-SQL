de8c846a68c8372061123376dd9468b6a3e609d6
Mooncakeproject <ao.carey@hotmail.com>
2024-01-31 18:49:41 +0700

    updated users table

--- users_table.sql
+++ users_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE users (
 	user_id SERIAL PRIMARY KEY,
 	name VARCHAR(50) NOT NULL,
 	last_name VARCHAR(50) NOT NULL,
-	email INT NOT NULL,
-	password INT NOT NULL,
+	email VARCHAR(50) NOT NULL,
+	password VARCHAR(50) NOT NULL,
 	profile VARCHAR(50)
 );
