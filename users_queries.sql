35d23405be49811226cfdec5bbcd909e7a748eb7
Mooncakeproject <ao.carey@hotmail.com>
2024-02-01 17:26:07 +0700

    updated_reviews_profile_query

--- users_queries.sql
+++ users_queries.sql
@@ -1,6 +1,6 @@
 -- users_registration query
 INSERT INTO users ( name, last_name, email, password, profile)
-VALUES ( 'Tommy', 'Gonzalez', 'tgonz@hot.com', 'haikuPass1', 'www.tgonzprofilepic@hot.com');
+VALUES ( 'Tommy', 'Gonzalez', 'tgonz@hot.com', 'haikuPass1', 'tgonzprofilepichot.com');
 -- login
 SELECT users.email, users.password
 	FROM users
@@ -13,9 +13,5 @@ WHERE
 	user_id = 10;
 -- update_profile_query
 UPDATE users
-SET profile = 'tgonzpic@hot.com', name = 'tommy', last_name = 'gonzalez', email = 'www.tgonz@hot.com'
+SET profile = 'https://tgonzpichot.com', name = 'tommy', last_name = 'gonzalez', email = 'tgonz@hotmail.com'
 WHERE user_id = 10;
-insert INTO
-    VALUES
-select 
-    from
