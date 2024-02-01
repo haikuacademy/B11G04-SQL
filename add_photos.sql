ba76818a73aaff67ca3e66aaf67495937258f512
therosstar <rosskirouac@gmail.com>
2024-02-01 17:43:33 +0700

    created add_photos

---
+++ add_photos.sql
@@ -0,0 +1,7 @@
+--  add photo to the table photo
+INSERT INTO photos (photo_id, url, house_id)
+VALUES
+    (1, 'https://blahblah.com/1', 1)
+    (2, 'https://blahblah.com/1', 1),
+    (3, 'https://blahblah.com/1', 1),
+    (4, 'https://blahblah.com/1', 1);
