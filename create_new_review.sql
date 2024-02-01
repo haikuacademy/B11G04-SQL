84d4530a1a3f73a148bd15ad9388662ac8abdba8
Mooncakeproject <ao.carey@hotmail.com>
2024-02-01 15:21:24 +0700

    modified read review query

--- create_new_review.sql
+++ create_new_review.sql
@@ -1,16 +1,19 @@
+-- create review
+
 INSERT INTO
 	reviews	(user_id,house_id, rating, content, date)
 	values (10, 1, 1, 'blahblah', '2024-03-01')
 ;
+-- read review
 
 SELECT 
-	reviews.user_id, reviews.house_id, rating, content, date
+	*
 FROM 
 	reviews
+WHERE
+	house_id = 1
 INNER JOIN 
-	houses 
+	reviews 
 	ON reviews.house_id = houses.house_id
-INNER JOIN users
-	 ON reviews.user_id = users.user_id
 ;
 
