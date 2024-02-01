782e8f7bac9ff5e28acf0ba4f09225805ccf323a
ValeriaSml <114675105+ValeriaSml@users.noreply.github.com>
2024-02-01 14:26:02 +0700

    read booking query

--- bookings_queries.sql
+++ bookings_queries.sql
@@ -1,3 +1,8 @@
+INSERT INTO bookings 
+	(house_id, user_id, price, arrival_date, departure_date, nights_number, photos.url)
+VALUES
+	(1, 10, 200, '2024-04-12', '2024-04-22', 10);
+
 SELECT bookings.house_id, bookings.user_id, bookings.price, bookings.arrival_date, 
 bookings.departure_date, bookings.nights_number, houses.bathrooms, 
 houses.bedrooms, houses.review_id, houses
