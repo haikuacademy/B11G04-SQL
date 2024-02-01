8a8b57bfe2622555a8b332cabc7f09f6addcce7a
ValeriaSml <114675105+ValeriaSml@users.noreply.github.com>
2024-02-01 16:15:28 +0700

    Update bookings_queries.sql

--- bookings_queries.sql
+++ bookings_queries.sql
@@ -1,14 +1,16 @@
+--create booking
+
 INSERT INTO bookings 
-	(house_id, user_id, price, arrival_date, departure_date, nights_number, photos.url)
+	(house_id, user_id, price, arrival_date, departure_date, nights_number)
 VALUES
 	(1, 10, 200, '2024-04-12', '2024-04-22', 10);
 
-SELECT bookings.house_id, bookings.user_id, bookings.price, bookings.arrival_date, 
-bookings.departure_date, bookings.nights_number, houses.bathrooms, 
-houses.bedrooms, houses.review_id, houses
+-- read booking query
+
+SELECT bookings.price, bookings.arrival_date, bookings.departure_date, bookings.comment
 FROM 
 	bookings
-	JOIN houses
-	ON bookings.house_id = houses.house_id
 	JOIN users
-	ON bookings.user_id = users.user_id;
+	ON bookings.user_id = users.user_id
+WHERE
+    bookings.user_id = 10
