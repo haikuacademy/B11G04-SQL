5b50b3ab9b7a74e6a87c87cb85b18df4a12e251e
Mooncakeproject <ao.carey@hotmail.com>
2024-02-01 14:29:23 +0700

    created_read_review_query

--- create_new_review.sql
+++ create_new_review.sql
@@ -1,3 +1,8 @@
+INSERT INTO
+	reviews	(user_id,house_id, rating, content, date)
+	values (10, 1, 1, 'blahblah', '2024-03-01')
+;
+
 SELECT 
 	reviews.user_id, reviews.house_id, rating, content, date
 FROM 
