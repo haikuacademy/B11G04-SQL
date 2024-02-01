266cc1988954bb9b52d39b8af3903f303ef74167
Mooncakeproject <ao.carey@hotmail.com>
2024-02-01 14:58:53 +0700

    corrected_queries

--- users_queries.sql
+++ users_queries.sql
@@ -1,17 +1,27 @@
 -- users_registration query
+
 INSERT INTO users ( name, last_name, email, password, profile)
-VALUES ( 'Tommy', 'Gonzalez', 'tgonz@hot.com', 'haikuPass1', 'www.tgonzprofilepic@hot.com');
+VALUES ( 'Tommy', 'Gonzalez', 'tgonz@hot.com', 'haikuPass1', 'https://www.tgonzprofilepic.hot.com')
+;
+
 -- login
+
 SELECT users.email, users.password
 	FROM users
 	WHERE user_id = 10;
+
 -- profile_query
-SELECT users.profile, users."name", users.last_name, users.email
+
+SELECT users.profile, users.name, users.last_name, users.email
 FROM
 	users
 WHERE
-	user_id = 10;
+	user_id = 10
+;
+
 -- update_profile_query
+
 UPDATE users
 SET profile = 'tgonzpic@hot.com', name = 'tommy', last_name = 'gonzalez', email = 'www.tgonz@hot.com'
-WHERE user_id = 10;
+WHERE user_id = 10
+;
